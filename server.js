5da2ec8fd960d16b10f7b44a3a052699b845fbb5
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2020-12-04 14:55:25 +0300

    Meska

---
+++ server.js
@@ -0,0 +1,179 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const moment = require('moment');
+var Jimp = require('jimp');
+const { Client, Util } = require('discord.js');
+const fs = require('fs');
+const db = require('quick.db');
+const http = require('http');
+const express = require('express');
+require('./util/eventLoader.js')(client);
+const path = require('path');
+const snekfetch = require('snekfetch');
+
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + "Paradox Code Pingledi");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+    console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("MANAGE_MESSAGES")) permlvl = 1;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+    console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+    console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
+
+//Eklendim-Atıldım/
+client.on('guildDelete', guild => {
+
+let embed = new Discord.MessageEmbed()
+
+.setColor("RED")
+.setTitle(" Bot Kickledi ")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.cache.get('780816057857998899').send(embed);
+
+});
+
+
+client.on('guildCreate', guild => {
+
+let embed = new Discord.MessageEmbed()
+
+.setColor("GREEN")
+.setTitle(" Bot Eklendi ")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.cache.get('780816057857998899').send(embed);
+
+}); 
+
+client.on('message', msg => {
+let sa = msg.content.toLowerCase()
+if (sa === 'lie yardım') {
+  const embed = new Discord.MessageEmbed()
+  .setColor('#d295a4')
+  .setTitle('lie')
+  .setDescription(`bir komut çalıştırmak için lie komut
+  Toplam 6 komut bulunuyor.
+  :flag_us: I'm so sorry, there is no English language option. Maybe soon ;)
+  
+  ***Komutlar:***
+  yardım, eval, hata-bildir, istatistik, uptime, v11tov12
+  
+ ***Linkler:***
+Website: Çok yakında!
+Destek Sunucum: [Tıkla](link)!
+Invite: [Tıkla!](link)`)
+msg.channel.send(embed)
+   }
+})
+
